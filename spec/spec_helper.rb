1f9b6d468f0767d2db50debd50765f7d3a6eb5df
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-05-17 15:29:15 +0930

    Moved custom matchers out of lib and into spec/support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,8 @@ Spork.prefork do
   # If you are not using ActiveRecord, you can remove this line.
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
   RSpec.configure do |config|
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
