0dba73a9002f47b1095852c6932f3a92b85bc6f0
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-06-27 15:17:05 +0930

    Update gem dependencies

--- spec/factories/micropost.rb
+++ spec/factories/micropost.rb
@@ -1,15 +1,4 @@
 FactoryGirl.define do
-  factory :user do
-    sequence(:name)  { |n| "Person #{n}" }
-    sequence(:email) { |n| "Person_#{n}@example.com" }
-    password "foobar"
-    password_confirmation "foobar"
-
-    factory :admin do
-      admin true
-    end
-  end
-
   factory :micropost do
     content { Faker::Lorem.sentence(5) }
     user
