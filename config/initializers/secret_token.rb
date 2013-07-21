4c43b0bb9c0db106b16ee52a8c65464fe0ef67b6
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-07-21 22:12:53 +1000

    Shorten secret_key_base to 117 characters so it can be used in Travis CI and deploy

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,23 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-# if Rails.env.production? && ENV['SECRET_KEY_BASE'].blank?
-#   raise 'SECRET_KEY_BASE environment variable must be set!'
-# end
-
-# SampleAppRails4::Application.config.secret_key_base =
-#   ENV['SECRET_KEY_BASE'] || 'x' * 30
-
-def secure_token
-  token_file = Rails.root.join('config/application.yml')
-  unless File.exist?(token_file)
-    %x(cp config/application.example.yml config/application.yml)
-    config = YAML.load_file(token_file)
-    token = %x(rake secret)
-    config['SECRET_KEY_BASE'] = token
-    File.open(token_file, 'w') { |f| YAML.dump(config, f) }
-  end
-  ENV['SECRET_KEY_BASE']
+if Rails.env.production? && ENV['SECRET_KEY_BASE'].blank?
+  raise 'SECRET_KEY_BASE environment variable must be set!'
 end
 
-SampleAppRails4::Application.config.secret_key_base = secure_token
+SampleAppRails4::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
