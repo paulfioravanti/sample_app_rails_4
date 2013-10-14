231b2c86f517033baa9ce2523b8bb5be6158f67d
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-10-15 01:59:52 +1100

    Test adding validation errors to Rollbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  after_validation :report_validation_errors_to_rollbar
 
   def self.authenticate(email, password)
     find_by(email: email).try(:authenticate, password)
