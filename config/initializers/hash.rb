4c37fae6ea3ce50044df452efebb7b82c8fc6f97
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-01-05 15:56:19 +1100

    Edit deprecation notice for Hash#diff

--- config/initializers/hash.rb
+++ config/initializers/hash.rb
@@ -1,12 +1,18 @@
-# This is the original Hash#diff method from Rails that will be depreciated.
+# This is the original Hash#diff method from Rails that will be deprecated.
+# It is here to stop the following notice:
+# DEPRECATION WARNING: Hash#diff is no longer used inside of Rails, and is
+# being deprecated with no replacement. If you're using it to compare hashes
+# for the purpose of testing, please use MiniTest's assert_equal instead.
+# (called from service at
+# /Users/paul/.rvm/rubies/ruby-2.0.0-p353/lib/ruby/2.0.0/webrick/httpserver.rb:138)
 #
-# Currently in rails 4.0.0.rc1, Webrick still uses the method, though
-# in 4.1.0.beta, it doesn't seem to.
+# Currently in rails 4.0.2, this issue occurs, though in 4.1.0.beta,
+# it doesn't seem to.
 # However, amongst the gems being used in this project, at least
 # Rails Footnotes and possibly other gems use this method, so it has been
 # ported here.
 #
-# The sole purpose of having this here at this time is to stop depreciation
+# The sole purpose of having this here at this time is to stop deprecation
 # notices, so if in the future there is a Hash#diff alternative that dependent
 # gems use, this can be removed.
 class Hash
